4b92c35baa41aff1c3a1b14ac8b8d68a1b2af98a
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-06-05 20:43:39 +0300

    created first project folder

---
+++ routing/firstProject/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about" element={About} />
+          <Route path="/users" element={Users} />
+          <Route path="/" element={Home} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
